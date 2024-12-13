06959e9b8563770c6ce35158370f04e125638d8f
Tristan Lacombe <tlacombevulca@outlook.fr>
2024-12-13 12:23:40 +0100

    Added translation support and translations for english and french - set french as the default language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent {}
+export class AppComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['fr_FR', 'en_EN']);
+    this.translate.setDefaultLang('fr_FR');
+    this.translate.use('fr_FR');
+  }
+}
